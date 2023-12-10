d433f1cb5c43a85e594cc1b73aa5a3430304ea99
munene-m <munenenjue18@gmail.com>
2023-12-10 17:48:12 +0300

    chore: added a delete user endpoint and protected some routes

--- routes/authRoute.ts
+++ routes/authRoute.ts
@@ -1,16 +1,19 @@
 import express from "express";
 const router = express.Router();
+import userProtect from "../middleware/authMiddleware";
 import {
   createUser,
   loginUser,
   registerAdmin,
   loginAdmin,
   getUsers,
+  deleteUser,
 } from "../controllers/authController";
 router.route("/register").post(createUser);
 router.route("/login").post(loginUser);
 router.route("/register-admin").post(registerAdmin);
 router.route("/login-admin").post(loginAdmin);
-router.route("/").get(getUsers);
+router.route("/").get(userProtect, getUsers);
+router.route("/delete/:id").delete(userProtect, deleteUser);
 
 export default router;
